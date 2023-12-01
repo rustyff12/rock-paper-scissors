7eff6edda87846189fc13c6f16ebbd6f47d19fd7
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2023-12-01 20:28:22 +0100

    js game function including score and new game added

--- js/main.js
+++ js/main.js
@@ -7,8 +7,9 @@
 // into rock, paper or scissors
 function getComputerChoice() {
 
-   let result = Math.floor(Math.random() * 3) + 1;
-    
+    // Random number generation for corresponding choices
+    let result = Math.floor(Math.random() * 3) + 1;
+
     if (result === 1) {
         result = "rock";
     } else if (result === 2) {
@@ -21,7 +22,6 @@ function getComputerChoice() {
     return result;
 
 }
-// getComputerChoice();
 
 
 // Function to take the game logic, what beats what. 
@@ -32,16 +32,57 @@ const playRound = (playerSelection, computerSelection) => {
         return "You Lose! Scissors beats Paper";
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
         return "You Lose! Rock beats Scissors";
-    } else if (playerSelection.toLowerCase() ===  computerSelection) {
+    } else if (playerSelection.toLowerCase() === computerSelection) {
         return "It's a Tie";
     } else {
         return `You Win! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}`;
     }
 }
 
-// console.log(playRound("scissors", "rock"));
 
+// play game function
+function game() {
+    // Scores set at 0
+    let playerScore = 0;
+    let computerScore = 0;
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+    // Loop for 5 rounds
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Please choose: rock, paper or scissors");
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+
+        console.log(result);
+
+        // Score increment on win
+        if (result.startsWith("You Win!")) {
+            playerScore++;
+        } else if (result.startsWith("You Lose!")) {
+            computerScore++;
+        }
+
+    }
+
+    // Returning the score
+    console.log(`Game over\n Your score was : ${playerScore}\n The computers score was: ${computerScore}\n`);
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations you win!");
+    } else if (computerScore > playerScore) {
+        console.log("Bad luck, the computer wins, better luck next time");
+    } else {
+        console.log("Oh, it's a tie");
+    }
+
+    // Ask if the user wants to play again
+    const playAgain = confirm("Would you like to play again?")
+
+    if (playAgain === true) {
+        game(); // Restarts game
+    } else {
+        console.log("Thanks very much for playing");
+    }
+}
+
+// Calls the beginning of the game
+game();
